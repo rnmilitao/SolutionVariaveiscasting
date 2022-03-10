be2313a326fa2452e91e09e478d4cd89e41dcc2b
Rozicleide <roziibr@hotmail.com>
2022-03-10 18:07:43 -0400

    Add project files.

---
+++ ConsoleApp6Variaveis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6Variaveis
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           // Casting implicito
+            int i = 10;
+            double d = i;
+
+            // Casting explicito
+            double d2 = 3.5;
+            int i2 = (int)d2;
+            Console.WriteLine(i2);
+        }
+    }
+}
